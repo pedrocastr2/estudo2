a1328928dfff45f5e4ce71acac62fe503a28f0f5
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-09-09 22:44:01 -0300

    Add files via upload

---
+++ estudodostiposdedados.py
@@ -0,0 +1,18 @@
+num1 = 3
+num2 = 2.5
+num3 = 1
+
+print(" o numero 1 é ..: " ,type(num1))
+print(" o numero 2 é ..: " ,type(num2))
+
+a = float(num1)
+print("agora numero 1 é...: ",a,"seu tipo é",type(a))
+
+b = int(num2)
+print("agora numero 2 é...: ",b,"seu tipo é",type(b))
+
+num3 = "1"
+print(" o numero 3 é ..: " ,num3,"seu tipo é",type(num3))
+
+d = int(float("4"))
+print('d é igual',d,'seu tipo é',type(d))
